b5f8aef9d2682d9cc474baf9fa85d05816a71e5b
mastamysta <benjamin-read@hotmail.co.uk>
2025-05-24 11:42:23 +0100

    Add initial implementation using linear probing. No hash function yet.

--- src/open_addr_map.hpp
+++ src/open_addr_map.hpp
@@ -1,3 +1,116 @@
 #pragma once
 
+#include <cstdint>
+#include <array>
+#include <iostream>
 
+template <typename K, typename V, uint32_t S>
+class OpenAddressMap
+{
+public:
+    using key_type = K;
+    using value_type = V;
+    using index_type = uint32_t;
+
+    auto operator[](key_type key) -> value_type&
+    {
+        index_type index = linear_probe_search(key);
+
+        if (index == -1)
+        {
+            // This is an insertion.
+            index = linear_probe_find_insert(key);
+            occupancy++;
+            key_store[index] = key;
+            used[index] = true;
+            deleted[index] = false;
+        }
+
+        return value_store[index];
+    }
+
+    auto operator[](key_type key) const -> const value_type&
+    {
+        index_type index = linear_probe_search(key);
+
+        if (index == -1)
+            return value_store.end();
+
+        return value_store[index];
+    }
+
+    auto remove(key_type key) -> bool
+    {
+        index_type index = linear_probe_search(key);
+
+        if (index == -1)
+            return false;
+
+        occupancy--;
+        deleted[index] = true;
+        return true;
+    }
+
+    auto get_occupancy() const -> const index_type&
+    {
+        return occupancy;
+    }
+
+private:
+    std::array<bool, S> used{};
+    std::array<bool, S> deleted{};
+    std::array<key_type, S> key_store{};
+    std::array<key_type, S> value_store{};
+    index_type occupancy = 0;
+    
+    auto linear_probe_find_insert(const key_type& k) const -> index_type
+    {
+        index_type index;
+        index_type attempt = 0;
+
+        while (true)
+        {
+            if (attempt >= S)
+                return -1;
+
+            index = get_hash(attempt, k);
+
+            if (!used[index] || deleted[index])
+                return index;
+
+            attempt++;
+
+        }
+    }
+
+    auto linear_probe_search(const key_type& k) const -> index_type
+    {
+        index_type index;
+        index_type attempt = 0;
+
+        while (true)
+        {
+            index = get_hash(attempt, k);
+
+            if (!used[index])
+                return -1;
+
+            if (key_store[index] == k && !deleted[index])
+                return index;
+
+            attempt++;
+
+        }
+    }
+
+    auto get_hash(uint32_t attempt, const key_type& k) const -> index_type
+    {
+        return (hash_key(k) + attempt) % S;
+    }
+
+    auto hash_key(const key_type& k) const -> index_type
+    {
+        return 0;
+    }
+
+};
--- testware/main.cpp
+++ testware/main.cpp
@@ -2,7 +2,33 @@
 
 #include "open_addr_map.hpp"
 
-TEST(DemoTestSuite, SomeTestCase)
+class OpenAddressMapFixture : public ::testing::Test
 {
-    ASSERT_TRUE(true);
+protected:
+    OpenAddressMap<int, int, 15> map;
+};
+
+TEST_F(OpenAddressMapFixture, CanInsertAndRetreive)
+{
+    map[10] = 5;
+    ASSERT_EQ(map[10], 5);
+}
+
+TEST_F(OpenAddressMapFixture, OccupancyOne)
+{
+    map[10] = 5;
+    ASSERT_EQ(map.get_occupancy(), 1);
+}
+
+
+TEST_F(OpenAddressMapFixture, OccupancyToZero)
+{
+    map[10] = 5;
+    map.remove(10);
+    ASSERT_EQ(map.get_occupancy(), 0);
+}
+
+TEST_F(OpenAddressMapFixture, CantDeleteNonExistent)
+{
+    ASSERT_FALSE(map.remove(10));
 }
